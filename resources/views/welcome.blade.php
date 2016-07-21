5298fec3dce5bbf54e1e06cd27ed7c8110778f74
Philippe Marcelin <philippejrm@gmail.com>
2016-07-20 23:07:10 -0400

    Login page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,44 +2,36 @@
 <html>
     <head>
         <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato', sans-serif;
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
+            {!!Html::style('css/material.min.css')!!}
+            {!!Html::script('js/material.min.js')!!}
+            {!!Html::script('js/styles.min.js')!!}
+            <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
         </style>
     </head>
     <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
+        <div class="mdl-card mdl-shadow--2dp">
+          <div class="mdl-card__title">
+            <h2 class="mdl-card__title-text">Login</h2>
+          </div>
+          {!! Form::open(array('url' => 'foo/bar')) !!}
+              <div class="mdl-card__supporting-text"> 
+                {!! Form::token(); !!}
+                <div class="mdl-textfield mdl-js-textfield">
+                    {!! Form::text('email', 'EMAIL', array('class' => 'mdl-textfield__input')); !!}
+                    {!! Form::label('email', 'EMAIL / USERNAME', array('class' => 'mdl-textfield__label')); !!}
+                </div>
+                <div class="mdl-textfield mdl-js-textfield">
+                    {!! Form::password('password', array('class' => 'mdl-textfield__input')); !!}
+                    {!! Form::label('password', 'PASSWORD', array('class' => 'mdl-textfield__label')); !!}
+                </div>  
+              </div>
+              <div class="mdl-card__actions mdl-card--border">
+                <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect pull-right">
+                  Login
+                </a>
+              </div>
+          {!! Form::close() !!}
         </div>
     </body>
 </html>
+<!----https://getmdl.io/started/index.html--->
