e932551ae5b41f2bcde79f34abee20f6bb0142e1
pockoface <pockoface@gmail.com>
2016-07-23 10:05:47 -0400

    email error fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,12 +5,12 @@
     <div class="mdl-card__title">
       <h2 class="mdl-card__title-text">Login</h2>
     </div>
-    {!! Form::open(array('url' => 'Auth\AuthController@postLogin)) !!}
+    {!! Form::open(array('url' => 'Auth\login', 'method' => 'post')) !!}
       {!! csrf_field() !!}
       <div class="mdl-card__supporting-text"> 
         {!! Form::token(); !!}
         <div class="mdl-textfield mdl-js-textfield">
-          {!! Form::text('email', 'EMAIL', array('class' => 'mdl-textfield__input')); !!}
+          {!! Form::text('email', '', array('class' => 'mdl-textfield__input')); !!}
           {!! Form::label('email', 'EMAIL / USERNAME', array('class' => 'mdl-textfield__label')); !!}
         </div>
         <div class="mdl-textfield mdl-js-textfield">
