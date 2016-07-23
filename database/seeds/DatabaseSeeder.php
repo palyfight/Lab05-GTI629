019380472b401098b5ce4c46d55602fe4b8a4d17
Philippe Marcelin <philippejrm@gmail.com>
2016-07-23 14:14:29 -0400

    fresh start

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'firstname' => "David",
+            'lastname' => "Delva",
+            'email' => 'dd@gmail.com',
+            'password' => bcrypt('random'),
+            'role' => "Admin",
+            'locked' => 0 
+        ]);
+
+        DB::table('users')->insert([
+            'firstname' => "Bob",
+            'lastname' => "Gratton",
+            'email' => 'bg@gmail.com',
+            'password' => bcrypt('random'),
+            'role' => "prepCercle",
+            'locked' => 0 
+        ]);
+
+        DB::table('users')->insert([
+            'firstname' => "Pernal Karl",
+            'lastname' => "Subban",
+            'email' => '76@gmail.com',
+            'password' => bcrypt('random'),
+            'role' => "prepCarre",
+            'locked' => 0 
+        ]);
+    }
+}
